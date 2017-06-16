b4a7400bad5b5ac3d5659d921629a76f66055f98
Mohit <mohitmotwani54@gmail.com>
2017-06-16 06:01:40 +0530

    Dataset and questions done

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -14,9 +14,42 @@
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
     
 """
+def greatest(a1, a, b1, b):
+    if(a>b):
+        return a1,a
+    else:
+        return b1,b
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+keys=enron_data.keys()
+#print enron_data
 
+
+#print keys
+# b1,b=greatest("COLWELL WESLEY", enron_data["COLWELL WESLEY"]["total_payments"],"FASTOW ANDREW S",enron_data["FASTOW ANDREW S"]["total_payments"])
+# print b1,b
+# print greatest("SKILLING JEFFREY K", enron_data["SKILLING JEFFREY K"]["total_payments"],b1,b)
+#print enron_data["FASTOW ANDREW S"]["total_payments"]
+count=0
+c=0
+#COLWELL WESLEY
+for k in keys:
+     if(enron_data[k]["poi"]==True):
+        c+=1
+        if(enron_data[k]["total_payments"]=="NaN"):
+            count+=1
+    # if (enron_data[k]["email_address"] != "NaN"):
+    #     c+= 1
+
+#print p
+# for k in keys:
+#    if(enron_data[k]["total_payments"]=="NaN"):
+#        count+=1
+
+#print len(enron_data)
+print count
+print c
+#print ((count+10)*100)/146
