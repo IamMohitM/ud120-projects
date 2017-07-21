eae3e796b16d977543cb8cc6b298744fdcbf0ba8
Mohit <mohitmotwani54@gmail.com>
2017-07-21 22:05:39 +0530

    regression with training data score

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -35,9 +36,11 @@ test_color = "r"
 from sklearn.linear_model import LinearRegression
 reg=LinearRegression()
 reg.fit(feature_train,target_train)
+pred=reg.predict(feature_train)
 print reg.coef_
 print reg.intercept_
-print reg.score(feature_test,target_test)
+print "r^2 with test data ", reg.score(feature_test,target_test)
+print "If using training data for r^2 ", reg.score(feature_train,target_train)
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
